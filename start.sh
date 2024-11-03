9833a3d2ed4fb5cff2ce9afe7ab79d47f8230e0f
doctoroyy <doctor.oyy@gmail.com>
2024-11-03 13:05:48 +0800

    feat: enable request logging in gunicorn
    - Add access and error logs to stdout
    - Set log level to info for better debugging
    - Keep logs visible in both direct run and Docker environment

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-gunicorn -w 4 -t 3600 -b 0.0.0.0:5000 main:app
+
+# Add access log and error log
+gunicorn -w 4 \
+    -t 3600 \
+    -b 0.0.0.0:5000 \
+    --access-logfile '-' \
+    --error-logfile '-' \
+    --log-level 'info' \
+    main:app
