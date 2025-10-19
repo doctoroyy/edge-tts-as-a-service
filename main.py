b92d929e0d80cee9fbbf9c8aeacd20e2295f8010
doctoroyy <doctor.oyy@gmail.com>
2025-10-19 14:58:30 +0800

    chore: revert api

--- main.py
+++ main.py
@@ -8,19 +8,19 @@ app = Flask(__name__)
 CORS(app, supports_credentials=True)
 
 
-async def stream_audio(text, voice, rate=None, volume=None, pitch=None) -> None: # type: ignore
-    communicate = edge_tts.Communicate(text, voice, rate=rate, volume=volume, pitch=pitch)
+async def stream_audio(text, voice) -> None:
+    communicate = edge_tts.Communicate(text, voice)
     for chunk in communicate.stream_sync():
         if chunk["type"] == "audio":
-            yield chunk["data"] # type: ignore
+            yield chunk["data"]
 
 
-def audio_generator(text, voice, rate=None, volume=None, pitch=None):
+def audio_generator(text, voice):
     loop = asyncio.new_event_loop()
-    coroutine = stream_audio(text, voice, rate, volume, pitch)
+    coroutine = stream_audio(text, voice)
     while True:
         try:
-            chunk = loop.run_until_complete(coroutine.__anext__()) # type: ignore
+            chunk = loop.run_until_complete(coroutine.__anext__())
             yield chunk
         except StopAsyncIteration:
             break
@@ -43,12 +43,8 @@ def tts():
     # voice not required
     voice = data.get('voice', 'zh-CN-YunxiNeural')
     file_name = data.get('file_name', OUTPUT_FILE)
-    # Optional parameters for speech customization
-    rate = data.get('rate')  # e.g., "+50%", "-50%"
-    volume = data.get('volume')  # e.g., "+50%", "-50%"
-    pitch = data.get('pitch')  # e.g., "+50Hz", "-50Hz"
 
-    communicate = edge_tts.Communicate(text, voice, rate=rate, volume=volume, pitch=pitch)
+    communicate = edge_tts.Communicate(text, voice)
     communicate.save_sync(file_name)
     return send_file(file_name, mimetype='audio/mpeg')
 
@@ -58,12 +54,8 @@ async def stream_audio_route():
     data = request.get_json()
     text = data['text']
     voice = data.get('voice', 'zh-CN-YunxiNeural')
-    # Optional parameters for speech customization
-    rate = data.get('rate')  # e.g., "+50%", "-50%"
-    volume = data.get('volume')  # e.g., "+50%", "-50%"
-    pitch = data.get('pitch')  # e.g., "+50Hz", "-50Hz"
 
-    return Response((audio_generator(text, voice, rate, volume, pitch)), content_type='application/octet-stream')
+    return Response((audio_generator(text, voice)), content_type='application/octet-stream')
 
 
 @app.route('/voices', methods=['GET'])
