87e16667491a86372443fa44508d57de6e70b8e1
doctoroyy <doctor.oyy@gmail.com>
2025-10-19 14:43:49 +0800

    chore: add optional parameters for audio customization in TTS functions

--- main.py
+++ main.py
@@ -8,19 +8,19 @@ app = Flask(__name__)
 CORS(app, supports_credentials=True)
 
 
-async def stream_audio(text, voice) -> None:
-    communicate = edge_tts.Communicate(text, voice)
+async def stream_audio(text, voice, rate='+0%', volume='+0%', pitch='+0Hz') -> None: # type: ignore
+    communicate = edge_tts.Communicate(text, voice, rate=rate, volume=volume, pitch=pitch)
     for chunk in communicate.stream_sync():
         if chunk["type"] == "audio":
-            yield chunk["data"]
+            yield chunk["data"] # type: ignore
 
 
-def audio_generator(text, voice):
+def audio_generator(text, voice, rate='+0%', volume='+0%', pitch='+0Hz'):
     loop = asyncio.new_event_loop()
-    coroutine = stream_audio(text, voice)
+    coroutine = stream_audio(text, voice, rate, volume, pitch)
     while True:
         try:
-            chunk = loop.run_until_complete(coroutine.__anext__())
+            chunk = loop.run_until_complete(coroutine.__anext__()) # type: ignore
             yield chunk
         except StopAsyncIteration:
             break
@@ -43,8 +43,12 @@ def tts():
     # voice not required
     voice = data.get('voice', 'zh-CN-YunxiNeural')
     file_name = data.get('file_name', OUTPUT_FILE)
+    # Optional parameters for speech customization
+    rate = data.get('rate')  # e.g., "+50%", "-50%"
+    volume = data.get('volume')  # e.g., "+50%", "-50%"
+    pitch = data.get('pitch')  # e.g., "+50Hz", "-50Hz"
 
-    communicate = edge_tts.Communicate(text, voice)
+    communicate = edge_tts.Communicate(text, voice, rate=rate, volume=volume, pitch=pitch)
     communicate.save_sync(file_name)
     return send_file(file_name, mimetype='audio/mpeg')
 
@@ -54,8 +58,12 @@ async def stream_audio_route():
     data = request.get_json()
     text = data['text']
     voice = data.get('voice', 'zh-CN-YunxiNeural')
+    # Optional parameters for speech customization
+    rate = data.get('rate')  # e.g., "+50%", "-50%"
+    volume = data.get('volume')  # e.g., "+50%", "-50%"
+    pitch = data.get('pitch')  # e.g., "+50Hz", "-50Hz"
 
-    return Response((audio_generator(text, voice)), content_type='application/octet-stream')
+    return Response((audio_generator(text, voice, rate, volume, pitch)), content_type='application/octet-stream')
 
 
 @app.route('/voices', methods=['GET'])
